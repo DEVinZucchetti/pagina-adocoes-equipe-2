1149d6f08740c8fc2d0b2d2094b525b661018517
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-04 16:14:20 -0300

    feat: inclui titulo, paragrafos e imagem ao projeto

--- src/views/Gabriel/Home.vue
+++ src/views/Gabriel/Home.vue
@@ -1,9 +1,51 @@
 <template>
-  <h1 class="title">Me leve para casa</h1>
+  <div class="content">
+    <div class="left-content">
+      <h1 class="title">Me leve para casa</h1>
+      <p class="text">
+        Nós cuidamos deles. <span class="highlighted"> Agora deixe que eles cuidem de você.</span>
+      </p>
+      <p class="text">
+        Aqui a gente cuida de animais resgatados de áreas evacuadas de Minas Gerais e queremos
+        compartilhar todo esse amor com mais pessoas por meio da adoção.
+      </p>
+      <p class="text">
+        Ter um animal de estimação traz benefícios para a saúde física e mental. É uma troca de amor
+        e carinho que faz bem pra todo mundo. Leve um para casa: cuide dele e deixe que ele cuide de
+        você também.
+      </p>
+      <div class="right-content">
+        <img
+          src="https://vale.com/documents/d/guest/imagem-dogs01?doAsUserId=RPhnXhXIUPuTnXbIjnHTCg%3D%3D%2F-%2Fcategories%2F64940%2F-%2Fcategories%2F%2F-%2Fcategories%2F64940%2F-%2Fcategories%2F%2F-%2Fcategories%2F%2F-%2Fcategories%2F64940%2F-%2Fcategories%2F%2F-%2Fcategories%2F%2F-%2Fcategories%2F%2F-%2Fcategories%2F64940%2F-%2Fcategories%2F64940%2F-%2Fcategories%2F%2F-%2Fcategories%2F%2F-%2Fcategories%2F"
+          class="content-image"
+        />
+      </div>
+    </div>
+  </div>
 </template>
-<style>
+<style scoped>
 .title {
   font-size: 32px;
   color: coral;
+}
+
+.text {
+  font-size: 16px;
+  text-align: justify;
+}
+
+.highlighted {
+  font-weight: bold;
+}
+
+.content {
+  display: flex;
+  width: 70%;
+  margin: 0 auto;
+  align-items: center;
+}
+
+.content-image {
+  width: 500px;
 }
 </style>
