94226c2190890c2f29010673a5702107cd3a892f
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-11 16:39:38 -0300

    feat: ajusta codigo que estava com bug

--- src/views/Gabriel/Pets.spec.js
+++ src/views/Gabriel/Pets.spec.js
@@ -1,22 +1,20 @@
-import { describe, it, expect, vi } from 'vitest'
-import { mount, flushPromises } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
+import { describe, expect, it, vi } from 'vitest'
 import Pets from './Pets.vue'
-import PetService from '../../services/PetService'
+import PetService from '@/services/PetService'
 
-describe('Tela de listagem de Pets', () => {
+describe('Tela de listagem de pets', () => {
   vi.spyOn(PetService, 'getAllPets').mockResolvedValue([
-    [
-      {
-        id: 1,
-        pet_name: 'Thor',
-        age: 5
-      },
-      {
-        id: 2,
-        pet_name: 'Java',
-        age: 5
-      }
-    ]
+    {
+      id: 1,
+      pet_name: 'Thor',
+      age: 5
+    },
+    {
+      id: 2,
+      pet_name: 'Java',
+      age: 5
+    }
   ])
 
   it('Espera-se que a tela seja renderizada', () => {
@@ -29,9 +27,11 @@ describe('Tela de listagem de Pets', () => {
     const component = mount(Pets)
 
     await flushPromises()
+
     expect(component.text()).toContain('Thor')
     expect(component.text()).toContain('Java')
-    expect(component.findAll("[data-test='item-pet'")).toHaveLength(2)
+
+    expect(component.findAll("[data-test='item-pet']")).toHaveLength(2)
   })
 
   it('Espera-se que navegue para a tela de perfil do pet clicado', async () => {
@@ -48,5 +48,9 @@ describe('Tela de listagem de Pets', () => {
     })
 
     await flushPromises()
+
+    component.findAll("[data-test='item-pet']")[0].trigger('click')
+
+    expect(mockRouter.push).toHaveBeenCalledWith('/pets-adocao/1/perfil')
   })
 })
