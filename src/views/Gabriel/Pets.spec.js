4d11676cb3fbcebc07955aebf092622bd3d19e12
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-11 11:30:18 -0300

    feat: faz o spyon do petservice

--- src/views/Gabriel/Pets.spec.js
+++ src/views/Gabriel/Pets.spec.js
@@ -1,8 +1,28 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import { mount } from '@vue/test-utils'
 import Pets from './Pets.vue'
-
+import PetService from '@/services/PetService'
 describe('Tela de listagem de Pets', () => {
+  vi.spyOn(PetService, 'getAllPets').mockResolvedValue([
+    [
+      {
+        id: 15,
+        pet_name: 'Atum',
+        age: 11
+      },
+      {
+        id: 14,
+        pet_name: 'Mutano',
+        age: 3
+      },
+      {
+        id: 13,
+        pet_name: 'Dora',
+        age: 7
+      }
+    ]
+  ])
+
   it('Espera-se que a tela seja renderizada', () => {
     const component = mount(Pets)
 
