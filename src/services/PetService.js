ce98f044e2c51fc315c8fbd70337e2ff6a96c9d5
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-11 11:17:48 -0300

    feat: cria um service para listagem de pets

---
+++ src/services/PetService.js
@@ -0,0 +1,11 @@
+import axios from 'axios'
+
+class PetService {
+  async getAllPets() {
+    const response = await axios.get('http://localhost:8000/api/pets/adocao')
+
+    return response.data
+  }
+}
+
+export default new PetService()
