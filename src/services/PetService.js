c56badd6ca448669c4dfab46dd99c93c895c6d75
GabrielMetz <135287636+GabrielmValadao@users.noreply.github.com>
2024-02-11 22:24:21 -0300

    feat: cria o getOnePet dentro do petservice

--- src/services/PetService.js
+++ src/services/PetService.js
@@ -3,7 +3,11 @@ import axios from 'axios'
 class PetService {
   async getAllPets() {
     const response = await axios.get('http://localhost:8000/api/pets/adocao')
+    return response.data
+  }
 
+  async getOnePet(id) {
+    const response = await axios.get(`http://127.0.0.1:8000/api/pets/${id}`)
     return response.data
   }
 }
