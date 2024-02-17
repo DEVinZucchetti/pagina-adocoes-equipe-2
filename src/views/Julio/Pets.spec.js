85cd3bd96ad7fbf0b4331790c2e372639b558b80
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-17 10:33:54 -0300

    Feat. Adiciona teste de roteamento de tela ao entrar no perfil do pet

--- src/views/Julio/Pets.spec.js
+++ src/views/Julio/Pets.spec.js
@@ -34,4 +34,22 @@ describe("Testa a tela de Listagem de Pets", () => {
         expect(component.findAll("[data-test='item-pet']")).toHaveLength(2)
     })
 
+    it("Espera-se que a tela navegue o perfil do pet clicado",async () => {
+
+        const mockRouter = {
+            push: vi.fn()
+        }
+        const component = mount(Pets,{
+            global: {
+                mocks:{
+                    $router: mockRouter
+                }
+            }
+        })
+        await flushPromises()
+        component.findAll("[data-test='item-pet']")[0].trigger("click")
+
+        expect(mockRouter.push).toHaveBeenCalledWith('/pets-adocao-4/1/perfil')
+    })
+
 })
