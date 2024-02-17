5daf2fa54542701f37effc28dc6d50288f5e8c0f
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-02-16 22:25:00 -0300

    Feat. Adiciona teste de renderizacao tela Home

---
+++ src/views/Julio/Home.spec.js
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import { flushPromises, mount } from "@vue/test-utils";
+
+import Home from '../Julio/Home.vue'
+
+describe("Testa a tela de aprendizado", () => {
+
+    it('Espera-se que que tela seja renderizada', () => {
+        const component = mount(Home)
+        expect(component).toBeTruthy()
+    })
+
+})
